71b1c866bbb3574a0dc64b7eebb4b5a42c8beb57
Jurioli <sniperlegend@hotmail.com>
2021-08-21 10:05:25 +0800

    1.1.8.6

---
+++ Blazor.WebForm.Components/Base/BaseCompareValidatorControlComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Blazor.WebForm.UI.ControlComponents
+{
+    public abstract class BaseCompareValidatorControlComponent<TControl> : BaseValidatorControlComponent<TControl>
+        where TControl : BaseCompareValidator, new()
+    {
+        [Parameter]
+        public ValidationDataType Type
+        {
+            get
+            {
+                return this.Control.Type;
+            }
+            set
+            {
+                this.Control.Type = value;
+            }
+        }
+
+        [Parameter]
+        public bool CultureInvariantValues
+        {
+            get
+            {
+                return this.Control.CultureInvariantValues;
+            }
+            set
+            {
+                this.Control.CultureInvariantValues = value;
+            }
+        }
+    }
+}
