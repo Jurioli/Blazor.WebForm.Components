ef8013f9034e5ad60e9bf788e46ffdbeea640a7b
Jurioli <sniperlegend@hotmail.com>
2021-08-30 09:25:16 +0800

    1.1.9.2

--- Blazor.WebForm.Components/Base/ControlParameterViewComponentExtensions.cs
+++ Blazor.WebForm.Components/Base/ControlParameterViewComponentExtensions.cs
@@ -182,6 +182,19 @@ namespace Blazor.WebForm.UI
             }
         }
 
+        public static ITemplate GetEditTemplateProperty<TValue>(this IParameterViewComponent component, RenderFragment<TValue> value)
+            where TValue : class, new()
+        {
+            if (value != null)
+            {
+                return new RenderFragmentBindableTemplateBuilder<TValue>(value, ExtractValues, context => new TValue().Apply(() => context));
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         private static IOrderedDictionary ExtractValues<TValue>(Control container, TValue context)
         {
             return new OrderedDictionary().Apply(() => context);
--- Blazor.WebForm.Components/Inner/Common/EditItemTemplate.cs
+++ Blazor.WebForm.Components/Inner/Common/EditItemTemplate.cs
@@ -1,4 +1,5 @@
-﻿using Blazor.WebForm.UI.PropertyComponents;
+﻿using Blazor.WebForm.UI;
+using Blazor.WebForm.UI.PropertyComponents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +9,26 @@ using System.Threading.Tasks;
 namespace Microsoft.AspNetCore.Components
 {
     public partial class EditItemTemplate<TItem> : BindableTemplatePropertyComponent<object, TItem>, ICommonPropertyComponent
+       where TItem : class, new()
     {
+        [Parameter]
+        public bool SubstituteItem { get; set; } = true;
+
         protected override string PropertyName
         {
             get
             {
                 return nameof(EditItemTemplate<TItem>);
             }
+        }
+
+        protected override KeyValuePair<string, object>? OnConvertParameter(KeyValuePair<string, object> parameter)
+        {
+            if (parameter.Key == nameof(this.ChildContent) && this.SubstituteItem)
+            {
+                return new KeyValuePair<string, object>(this.PropertyName, this.GetEditTemplateProperty(this.ChildContent));
+            }
+            return base.OnConvertParameter(parameter);
         }
     }
 }
